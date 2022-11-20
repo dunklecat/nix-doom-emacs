308144cd3cafd8b4155bc6ad333209e023b87308
dunklecat <me@dunklecat.dev>
2022-11-20 11:00:39 +0100

    fix

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -738,7 +738,7 @@ in {
         texlive.combined.scheme-medium # Should it be full?
         (mkIf (elem "+gnuplot" cfg.doomInit.lang.org.flags) gnuplot)
         (mkIf ((elem "+roam2" cfg.doomInit.lang.org.flags)
-          || (elem "+roam" cfg.doomInit.lang.org.flags) sqlite))
+          || (elem "+roam" cfg.doomInit.lang.org.flags)) sqlite)
         (mkIf (elem "+jupiter" cfg.doomInit.lang.org.flags)
           (python3.withPackages (ps: with ps; [ jupyter ])))
       ];
